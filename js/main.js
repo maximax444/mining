649576d0a7575615f2ec5d4a0fc3dc4b20bbea46
Max <max.kuzn.msx@gmail.com>
2021-10-09 11:07:34 +0300

    add product card

--- js/main.js
+++ js/main.js
@@ -13,6 +13,12 @@
                 .closest('.home-info').find('.home-info__cont-block').removeClass('active').eq($(this).index()).addClass('active');
         });
 
+        $('.product-info__tabs').on('click', '.product-info__tabs-block:not(.active)', function () {
+            $(this)
+                .addClass('active').siblings().removeClass('active')
+                .closest('.product-info').find('.product-info__cont-block').removeClass('active').eq($(this).index()).addClass('active');
+        });
+
     });
 })(jQuery);
 $('.home-offer__range-title').css('left', 40 / 2 + "%");
@@ -26,4 +32,25 @@ document.querySelectorAll('.home-offer__range').forEach(inp => {
 });
 $('.home-faq__name').on("click", function () {
     $(this).closest('.home-faq__block').toggleClass("active");
+});
+$('.product-left__slider img').on("click", function () {
+    $('.product-left__img img').attr("src", $(this).attr("src"));
+});
+$('.product-left__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    slide: 'img',
+    prevArrow: ".product-left__slider-prev",
+    nextArrow: ".product-left__slider-next",
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false
+        }
+    }]
 });
